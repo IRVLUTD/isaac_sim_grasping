c9544b33f750af8c21006b90353bfe31eb1fbc2c
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2023-12-08 16:44:33 -0600

    scripts for creating urdf and usd for objects

---
+++ create_urdf.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Prepare Google Scan-3D objects URDFs for Pybullet/IsaacSim',
+    usage="\
+    python google_prepare_urdf.py -f /PATH/TO/GOOGLE_DATA_FOLDER/ -m model_list.txt'")
+
+parser.add_argument('-m', '--models_file', type=str, required=True, default='model_list.txt', help="List of object names to export to URDF")
+parser.add_argument('-f', '--google_dir', type=str, required=True, default='/mnt/Data/GoogleScannedObjects/SampleObjects/', help='Path to GoogleScan3d Models Directory.')
+
+
+def get_urdf_file(ycb_model: str, mesh_path: str) -> str:
+    URDF_TEMPLATE = '''<?xml version='1.0' ?>
+<robot name="{ycb_model_name}">
+    <link name="base_link">
+        <visual>
+            <origin rpy="0 0 0" xyz="0 0 0"/>
+            <geometry>
+                <mesh filename="{ycb_model_mesh_path}" scale="1.0 1.0 1.0"/>
+            </geometry>
+            <material name="texture">
+                <color rgba="1.0 1.0 1.0 1.0" />
+            </material>
+        </visual>
+        <collision>
+            <origin rpy="0 0 0" xyz="0 0 0"/>
+            <geometry>
+                <mesh filename="{ycb_model_mesh_path}" scale="1.0 1.0 1.0"/>
+            </geometry>
+        </collision>
+        <inertial>
+            <!-- <mass value="1.0"/> -->
+            <inertia ixx="1.0" ixy="0.0" ixz="0.0" iyy="1.0" iyz="0.0" izz="1.0" />
+        </inertial>
+    </link>
+</robot>'''
+    return URDF_TEMPLATE.format(ycb_model_name=ycb_model, ycb_model_mesh_path=mesh_path)
+
+
+def main(args):
+
+    if args.models_file and not os.path.isfile(args.models_file):
+        print(f"File does not exist: {args.models_file}")
+        exit(0)
+
+    if not args.google_dir:
+        print(f"models list not specified")
+        exit(0)
+
+    if not os.path.isdir(args.google_dir):
+        print(f"models directory (containing meshes): {args.google_dir} is incorrect")
+        exit(0)
+
+    with open(args.models_file) as f:
+        model_names = f.read().splitlines()
+
+    for model in model_names:
+        model_dir = os.path.join(args.google_dir, model) 
+        mesh_p =  os.path.join("meshes", "model.obj")
+        urdf_content = get_urdf_file(model, mesh_p)
+        with open(os.path.join(model_dir, f"{model}.urdf"), "w") as outf:
+            outf.write(urdf_content)
+        # NOTE: We write another file with same content, but its titled model.urdf similar to the model.sdf in each object's folder
+        with open(os.path.join(model_dir, "model.urdf"), "w") as outf:
+            outf.write(urdf_content)
+
+
+if __name__ == '__main__':
+    main(parser.parse_args())
