188a2649d60fe8a4d3a8810a4aa9cd775e9e4d27
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2023-12-12 08:40:38 -0600

    mass in urdf via oriented bbox

--- create_urdf.py
+++ create_urdf.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-
+import trimesh
+from functools import reduce
 parser = argparse.ArgumentParser(description='Prepare Google Scan-3D objects URDFs for Pybullet/IsaacSim',
     usage="\
     python google_prepare_urdf.py -f /PATH/TO/GOOGLE_DATA_FOLDER/ -m model_list.txt'")
@@ -9,7 +10,7 @@ parser.add_argument('-m', '--models_file', type=str, required=True, default='mod
 parser.add_argument('-f', '--google_dir', type=str, required=True, default='/mnt/Data/GoogleScannedObjects/SampleObjects/', help='Path to GoogleScan3d Models Directory.')
 
 
-def get_urdf_file(ycb_model: str, mesh_path: str) -> str:
+def get_urdf_file(ycb_model: str, mesh_path: str, mass: float) -> str:
     URDF_TEMPLATE = '''<?xml version='1.0' ?>
 <robot name="{ycb_model_name}">
     <link name="base_link">
@@ -29,12 +30,12 @@ def get_urdf_file(ycb_model: str, mesh_path: str) -> str:
             </geometry>
         </collision>
         <inertial>
-            <!-- <mass value="1.0"/> -->
+            <!-- <mass value="{mass_value}"/> -->
             <inertia ixx="1.0" ixy="0.0" ixz="0.0" iyy="1.0" iyz="0.0" izz="1.0" />
         </inertial>
     </link>
 </robot>'''
-    return URDF_TEMPLATE.format(ycb_model_name=ycb_model, ycb_model_mesh_path=mesh_path)
+    return URDF_TEMPLATE.format(ycb_model_name=ycb_model, ycb_model_mesh_path=mesh_path, mass_value=mass)
 
 
 def main(args):
@@ -54,10 +55,16 @@ def main(args):
     with open(args.models_file) as f:
         model_names = f.read().splitlines()
 
+    DENSITY = 500
     for model in model_names:
         model_dir = os.path.join(args.google_dir, model) 
         mesh_p =  os.path.join("meshes", "model.obj")
-        urdf_content = get_urdf_file(model, mesh_p)
+        obj = trimesh.load(os.path.join(model_dir, mesh_p))
+        _, extents = trimesh.bounds.oriented_bounds(obj)
+        volume = reduce(lambda x, y: x*y, extents)
+        mass = volume * DENSITY
+        print(f"{model}: BBox {extents} ; VOL={volume} ; MASS = {volume * DENSITY} Kg")
+        urdf_content = get_urdf_file(model, mesh_p, mass)
         with open(os.path.join(model_dir, f"{model}.urdf"), "w") as outf:
             outf.write(urdf_content)
         # NOTE: We write another file with same content, but its titled model.urdf similar to the model.sdf in each object's folder
